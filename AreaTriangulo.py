dfb7831a7ea2ca3f8872038dcf1711e73e8c1052
JhampierGZ <>
2021-04-26 16:03:33 +0000

    holi mundo

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Definicion de variables y otros
+print("ajercicio 01: Area de un Trianfulo ")
+#Datos de entrada mediante dispositivos de entrada
+b=int(input("Ingrese Base:"))
+h=int(input("Ingrese altura:"))
+#preceso
+area=(b*h)/2
+#datos de salida
+print("El area es:", area)
